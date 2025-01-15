40fdd5e0bd7e845040e69a840a371c487a7b511c
frederikkruusmae <115454921+frederikkruusmae@users.noreply.github.com>
2025-01-15 15:49:50 +0200

    Create install_docker.sh

---
+++ prometheus_lab_part1/install_docker.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+DOCKER_COMPOSE_VERSION="1.29.2"
+sudo apt-get update
+sudo apt-get install -y apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+sudo apt-get update
+sudo apt-get install -y docker-ce
+sudo usermod -aG docker $USER
+sudo curl -L "https://github.com/docker/compose/releases/download/${DOCKER_COMPOSE_VERSION}/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+docker --version
+docker-compose --version
+echo "Please log out and log back in to apply the Docker group changes, or run: newgrp docker"
